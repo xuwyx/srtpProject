bfd06fe57f5bfd57ca47bf9d12ec47baa0d53c77
Jcc12 <jocevelqjy@163.com>
2018-05-22 23:17:36 +0800

    update

--- Demo.py
+++ Demo.py
@@ -10,7 +10,7 @@ class Classification:
         self.mean = pkl.load(open("mean", "rb"))
         self.std = pkl.load(open("std", "rb"))
         self.model_A = models.load_model("model_a")
-        self.model_V = models.load_model("model_v")  #8 is better
+        self.model_V = models.load_model("model_v")  
         self.valance = 0
         self.arousal = 0
 
@@ -28,14 +28,14 @@ class Classification:
             else:
                 data_x.append(my_matrix[716:732, :])
         # data_x.append(my_matrix[76:292, :])
-        # data_x -= self.mean
-        # data_x /= self.std
+        data_x -= np.mean(data_x, axis=0)
+        data_x /= np.std(data_x, axis=0)
         data_x = np.array(data_x)
         print(data_x.shape)
         data_x = data_x[0:64][:, :]
         classes_V = self.model_V.predict(data_x, batch_size=self.batch_size)
         classes_A = self.model_A.predict(data_x, batch_size=self.batch_size)
-        self.valance =1 / classes_V[0] * 0.01
+        self.valance = classes_V[0] * 10
         self.arousal = classes_A[0] * 10
 
         return 1
